d3bcf97897f563adc340d5b1cde33cdb88b66717
Eric Torti <etorti@sp.r7.com>
2014-05-09 20:53:16 -0300

    getting a hang of how lambdas work in ruby

--- lib/cachinguele/redefiner.rb
+++ lib/cachinguele/redefiner.rb
@@ -4,13 +4,12 @@ class Cachinguele::Redefiner
   
   REDEFINER_PREFIX = 'cachinguele_redefined'
 
-  def self.redefine_methods(klass, method_names, wrap_around)
+  def self.redefine_method(klass, method_name, wrap_around)
     klass.class_eval do
-      method_names.each do |method_name|
-        alias_method "#{REDEFINER_PREFIX}_#{method_name}".to_sym, method_name 
-        define_method method_name do
-          wrap_around.call(klass, method_name, lambda{ send("#{REDEFINER_PREFIX}_#{method_name}".to_sym()) })
-        end
+      alias_method "#{REDEFINER_PREFIX}_#{method_name}".to_sym, method_name 
+      define_method method_name do
+        wrap_around.call(klass, method_name)
+        send("#{REDEFINER_PREFIX}_#{method_name}".to_sym)
       end
     end
   end
--- spec/cachinguele/redefiner_spec.rb
+++ spec/cachinguele/redefiner_spec.rb
@@ -1,22 +1,55 @@
 require 'cachinguele/redefiner'
 
-class Cat 
-  def mew 
-    'meow'
+describe Cachinguele::Redefiner do
+  before :each do
+    class Cat 
+      def mew 
+        'meow'
+      end
+    end
+    expect(Cat.new.mew).to eq 'meow'
   end
 
-end
-
-
-describe Cachinguele::Redefiner do
-
-  it 'works' do 
-    wrap_around = lambda do |klass, method_name, original_method_as_lambda|
-      klass.should eql Cat
-      method_name.should eql method_name
-      original_method_as_lambda.call.should eql 'meow'
+  describe 'how lambdas work in Ruby' do
+    it 'works with a lambda disguised as a block' do
+      l = lambda {send(:redefined_mew)}
+      Cat.class_eval do
+        alias_method :redefined_mew, :mew
+        # evaluates the lambda in the context of the object on which :mew is called
+        define_method :mew, &l 
+      end
+      expect(Cat.new.mew).to eq 'meow'
     end
-    Cachinguele::Redefiner.redefine_methods(Cat, [:mew], wrap_around)
+
+    it 'does NOT work with a pure lambda ' do
+      l = lambda {send(:redefined_mew)}
+      Cat.class_eval do
+        alias_method :redefined_mew, :mew
+        define_method :mew do
+          # l retains the flat scope and raises NoMethodError for :cachinguele_redefined_mew
+          l.call
+        end
+      end
+      expect { Cat.new.mew }.to raise_error 
+    end
+  end 
+
+  context '#redefine_method' do
+    it 'calls a given block as follows' do
+      l = lambda do |klass, method_name|
+        expect(klass).to eq Cat
+        expect(method_name).to eq :mew 
+      end
+      Cachinguele::Redefiner.redefine_method(Cat, :mew, l)
+    end
+
+    context 'when original implementation needs to be left alone' do
+      it 'it leaves the original implementation untouched' do
+        l = lambda { |klass, method_name| } 
+        Cachinguele::Redefiner.redefine_method(Cat, :mew, l)
+        expect(Cat.new.mew).to eq 'meow'
+      end
+    end
   end
 
 end
